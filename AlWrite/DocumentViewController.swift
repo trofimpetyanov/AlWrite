7feadb15d5c72f6da0799576e62eb153d7164d51
tapetyanov <tapetyanov@yandex-team.ru>
2025-01-11 17:58:01 +0300

    Initial Commit

---
+++ AlWrite/DocumentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DocumentViewController.swift
+//  AlWrite
+//
+//  Created by Trofim Petianov on 11.01.2025.
+//
+
+import UIKit
+
+class DocumentViewController: UIDocumentViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
